e1ef55d63ccd84a04e1710314f7114314d88b839
Serkan Mehmet Malagiç <serkan.malagic@portalgrup.com>
2021-01-27 10:53:39 +0300

    Initial commit

---
+++ LoodosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosApp
+//
+//  Created by Serkan Mehmet Malagiç on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
